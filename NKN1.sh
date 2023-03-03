0d88e50197ca58f590594000d77fdc96aa171fc7
10641046 <106446830+10641046@users.noreply.github.com>
2023-03-03 17:32:00 +0800

    Create NKN1.sh

---
+++ NKN1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+dd if=/dev/zero of=/tmp/swap bs=1M count=4096&&mkswap /tmp/swap&&swapon /tmp/swap&&swapon -s&&
+wget https://download.npool.io/npool.sh&&sudo chmod +x npool.sh&&sudo ./npool.sh 2ja04P823ll6yCkm&&
+apt-get update&&apt install default-jre -y&&apt install unzip&&
+cd

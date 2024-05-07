84e7bfcebf794ee4d57e5f7c72e073bc8fe7854c
10641046 <106446830+10641046@users.noreply.github.com>
2024-05-07 17:21:00 +0800

    Update ga+titan.sh

--- ga+titan.sh
+++ ga+titan.sh
@@ -17,13 +17,13 @@ echo "节点社区 Discord 社群:https://discord.gg/GbMV5EcNWF"
 id="EB88C971-3F33-4BF7-8D3C-360823A4913D"
 
 # 让用户输入想要创建的容器数量
-container_count="1"
+container_count="5"
 
 # 让用户输入起始 RPC 端口号
 start_rpc_port="30001"
 
 # 让用户输入想要分配的空间大小
-storage_gb="15"
+storage_gb="3"
 
 # 让用户输入存储路径（可选）
 #custom_storage_path="titan_storage_1"

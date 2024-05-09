1c6fe9d525a0ceced261aadffe76317a9f9fa254
10641046 <106446830+10641046@users.noreply.github.com>
2024-05-09 16:18:52 +0800

    Update network3.sh

--- network3.sh
+++ network3.sh
@@ -15,17 +15,6 @@ sudo bash manager.sh up
 
 
 done
-# 获取公网 IP 地址
-public_ip=$(curl -s ifconfig.me)
-
-# 准备原始链接
-original_url="https://account.network3.ai/main?o=${port_grafana}:8080"
-
-# 替换 LocalHost 为公网 IP 地址
-updated_url=$(echo $original_url | sed "s/LocalHost/$public_ip/")
-
-# 显示更新后的链接
-echo "请通过以下链接绑定设备：$updated_url"
 
 
 

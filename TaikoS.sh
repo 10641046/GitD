e0a8df282f0cf4912f6613633c53fb48c3194c92
10641046 <106446830+10641046@users.noreply.github.com>
2024-05-11 17:07:15 +0800

    Update TaikoS.sh

--- TaikoS.sh
+++ TaikoS.sh
@@ -37,7 +37,7 @@ fi
 l1_endpoint_http="http://138.201.221.84:8545"
 l1_endpoint_ws="ws://138.201.221.84:8546"
 l1_beacon_http="http://unstable.holesky.beacon-api.nimbus.team"
-prover_endpoints="http://kenz-prover.hekla.kzvn.xyz:9876,http://hekla.stonemac65.xyz:9876,http://taiko.web3crypt.net:9876,http://198.244.201.79:9876"
+prover_endpoints="http://198.244.201.79:9876,https://prover-hekla.taiko.tools,https://prover2-hekla.taiko.tools,http://taiko-a7-prover.zkpool.io,http://146.59.55.26:9876,http://kenz-prover.hekla.kzvn.xyz:9876,http://hekla.stonemac65.xyz:9876,http://51.91.70.42:9876,http://taiko.web3crypt.net:9876,http://148.113.17.127:9876,http://hekla.prover.taiko.coinblitz.pro:9876,http://taiko-testnet.m51nodes.xyz:9876,http://148.113.16.26:9876,http://51.161.118.103:9876,http://162.19.98.173:9876,http://49.13.215.95:9876,http://49.13.143.184:9876,http://49.13.210.192:9876,http://159.69.242.22:9876,http://49.13.69.238:9876,http://taiko.guru:9876,http://taiko.donkamote.xyz:9876"
 enable_proposer="true"
 read -p "请输入EVM钱包私钥,不需要带0x: " l1_proposer_private_key
 read -p "请输入EVM钱包地址: " l2_suggested_fee_recipient

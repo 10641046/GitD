5baa91fc599e33bafe0e205223bd3889908df5bf
10641046 <106446830+10641046@users.noreply.github.com>
2024-05-13 00:04:49 +0800

    Update TaikoD.sh

--- TaikoD.sh
+++ TaikoD.sh
@@ -15,28 +15,3 @@ fi
     cd #HOME
     rm -rf simple-taiko-node
 
-# 安装节点
-git clone https://github.com/taikoxyz/simple-taiko-node.git
-
-# 复制备份.env
-cp -a ~/t1/.env simple-taiko-node/
-
-# 运行节点
-cd simple-taiko-node
-docker compose --profile l2_execution_engine down
-docker stop simple-taiko-node-taiko_client_proposer-1 && docker rm simple-taiko-node-taiko_client_proposer-1
-docker compose --profile l2_execution_engine up -d
-docker compose --profile proposer up -d
-cd
-
-# 获取公网 IP 地址
-public_ip=$(curl -s ifconfig.me)
-
-# 准备原始链接
-original_url="LocalHost:${port_grafana}/d/L2ExecutionEngine/l2-execution-engine-overview?orgId=1&refresh=10s"
-
-# 替换 LocalHost 为公网 IP 地址
-updated_url=$(echo $original_url | sed "s/LocalHost/$public_ip/")
-
-# 显示更新后的链接
-echo "请通过以下链接查询设备运行情况，如果无法访问，请等待2-3分钟后重试：$updated_url"

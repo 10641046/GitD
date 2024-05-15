d590cc0f04fd4753884d22e5d5b78c7c1294c10f
10641046 <106446830+10641046@users.noreply.github.com>
2024-05-15 19:25:22 +0800

    Update TaikoW.sh

--- TaikoW.sh
+++ TaikoW.sh
@@ -11,5 +11,5 @@ fi
 cd simple-taiko-node&&docker compose  --profile l2_execution_engine down&&cd
 cd simple-taiko-node&&docker stop simple-taiko-node-taiko_client_proposer-1 && docker rm simple-taiko-node-taiko_client_proposer-1&&cd
 #运行节点
-cd simple-taiko-node&&docker compose --profile l2_execution_engine up -d&&docker compose --profile proposer up -d
-cd
+#cd simple-taiko-node&&docker compose --profile l2_execution_engine up -d&&docker compose --profile proposer up -d
+#cd

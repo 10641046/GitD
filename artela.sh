66f4d564de1e1a7a493faf1ae4768d5812992f60
10641046 <106446830+10641046@users.noreply.github.com>
2024-05-25 02:11:43 +0800

    Update artela.sh

--- artela.sh
+++ artela.sh
@@ -6,26 +6,102 @@ if [ "$(id -u)" != "0" ]; then
     echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
     exit 1
 fi
+# 检查并安装 Node.js 和 npm
+function install_nodejs_and_npm() {
+    if command -v node > /dev/null 2>&1; then
+        echo "Node.js 已安装"
+    else
+        echo "Node.js 未安装，正在安装..."
+        curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+        sudo apt-get install -y nodejs
+    fi
 
-#修改 beacon rpc
-#sed -i 's|^L1_BEACON_HTTP=.*|L1_BEACON_HTTP=http://unstable.holesky.beacon-api.nimbus.team|' ~/simple-taiko-node/.env
+    if command -v npm > /dev/null 2>&1; then
+        echo "npm 已安装"
+    else
+        echo "npm 未安装，正在安装..."
+        sudo apt-get install -y npm
+    fi
+}
 
-#修改 prover rpc
-#sed -i 's|^PROVER_ENDPOINTS=.*|PROVER_ENDPOINTS=http://kenz-prover.hekla.kzvn.xyz:9876,http://hekla.stonemac65.xyz:9876,http://taiko.web3crypt.net:9876,http://198.244.201.79:9876|' ~/simple-taiko-node/.env
+# 检查并安装 PM2
+function install_pm2() {
+    if command -v pm2 > /dev/null 2>&1; then
+        echo "PM2 已安装"
+    else
+        echo "PM2 未安装，正在安装..."
+        npm install pm2@latest -g
+    fi
+}
 
-#修改 GAS
-sed -i 's|^TX_GAS_LIMIT=.*|TX_GAS_LIMIT=9000000|' ~/simple-taiko-node/.env
-sed -i 's|^BLOCK_PROPOSAL_FEE=.*|BLOCK_PROPOSAL_FEE=60|' ~/simple-taiko-node/.env
+# 检查Go环境
+function check_go_installation() {
+    if command -v go > /dev/null 2>&1; then
+        echo "Go 环境已安装"
+        return 0 
+    else
+        echo "Go 环境未安装，正在安装..."
+        return 1 
+    fi
+}
+# 节点安装功能
 
+    # 设置变量
+    read -r -p "请输入你想设置的节点名称: " NODE_MONIKER
+    export NODE_MONIKER=$NODE_MONIKER
 
-#备份.env
-rm -rf t1&&mkdir t1&&cp -a ~/simple-taiko-node/.env /root/t1/
+    # 更新和安装必要的软件
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install -y curl iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev lz4 snapd
 
-#停止节点
-cd simple-taiko-node
-docker compose  --profile l2_execution_engine down
-docker stop simple-taiko-node-taiko_client_proposer-1 && docker rm simple-taiko-node-taiko_client_proposer-1&&cd
+    # 安装 Go
+    if ! check_go_installation; then
+        sudo rm -rf /usr/local/go
+        curl -L https://go.dev/dl/go1.22.0.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+        echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> /mnt/c1/.bash_profile
+        source /mnt/c1/.bash_profile
+        go version
+    fi
 
-#运行节点
-cd simple-taiko-node&&docker compose --profile l2_execution_engine up -d&&docker compose --profile proposer up -d
-cd
+    # 安装所有二进制文件
+    cd /mnt/c1
+    git clone https://github.com/artela-network/artela
+    cd artela
+    git checkout v0.4.7-rc6
+    make install
+
+    # 配置artelad
+    artelad config chain-id artela_11822-1
+    artelad init "$NODE_MONIKER" --chain-id artela_11822-1
+    artelad config node tcp://localhost:3457
+
+    # 获取初始文件和地址簿
+    curl -L https://snapshots-testnet.nodejumper.io/artela-testnet/genesis.json > /mnt/c1/.artelad/config/genesis.json
+    curl -L https://snapshots-testnet.nodejumper.io/artela-testnet/addrbook.json > /mnt/c1/.artelad/config/addrbook.json
+
+    # 配置节点
+    SEEDS=""
+    PEERS="096d8b3a2fe79791ef307935e0b72afcf505b149@84.247.140.122:24656,a01a5d0015e685655b1334041d907ce2db51c02f@173.249.16.25:45656,8542e4e88e01f9c95db2cd762460eecad2d66583@155.133.26.10:26656,dd5d35fb496afe468dd35213270b02b3a415f655@15.235.144.20:30656,8510929e6ba058e84019b1a16edba66e880744e1@217.76.50.155:656,f16f036a283c5d2d77d7dc564f5a4dc6cf89393b@91.190.156.180:42656,6554c18f24455cf1b60eebcc8b311a693371881a@164.68.114.21:45656,301d46637a338c2855ede5d2a587ad1f366f3813@95.217.200.98:18656"
+    sed -i 's|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' /mnt/c1/.artelad/config/config.toml
+
+    # 配置端口
+    node_address="tcp://localhost:3457"
+    sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:3458\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:3457\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:3460\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:3456\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":3466\"%" $HOME/.artelad/config/config.toml
+    sed -i -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:3417\"%; s%^address = \":8080\"%address = \":3480\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:3490\"%; s%^address = \"localhost:9091\"%address = \"0.0.0.0:3491\"%; s%:8545%:3445%; s%:8546%:3446%; s%:6065%:3465%" $HOME/.artelad/config/app.toml
+    echo "export Artela_RPC_PORT=$node_address" >> /mnt/c1/.bash_profile
+    source /mnt/c1/.bash_profile   
+
+    pm2 start artelad -- start && pm2 save && pm2 startup
+    
+    # 下载快照
+    artelad tendermint unsafe-reset-all --home /mnt/c1/.artelad --keep-addr-book
+    curl https://snapshots-testnet.nodejumper.io/artela-testnet/artela-testnet_latest.tar.lz4 | lz4 -dc - | tar -xf - -C /mnt/c1/.artelad
+    mv /mnt/c1/.artelad/priv_validator_state.json.backup /mnt/c1/.artelad/data/priv_validator_state.json
+
+    # 使用 PM2 启动节点进程
+
+    pm2 restart artelad
+
+    echo '====================== 安装完成,请退出脚本后执行 source $HOME/.bash_profile 以加载环境变量 ==========================='
+    
+}

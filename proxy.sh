33acc91e75ceaf4d6db63554e891e5b165859826
10641046 <106446830+10641046@users.noreply.github.com>
2024-05-25 20:52:09 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -8,31 +8,5 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 sudo apt update
-
-# 检查 Docker 是否已安装
-if ! command -v docker &> /dev/null
-then
-    # 如果 Docker 未安装，则进行安装
-    echo "未检测到 Docker，正在安装..."
-    sudo apt-get install ca-certificates curl gnupg lsb-release
-
-    # 添加 Docker 官方 GPG 密钥
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-    # 设置 Docker 仓库
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    # 授权 Docker 文件
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
-    sudo apt-get update
-
-    # 安装 Docker 最新版本
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-else
-    echo "Docker 已安装。"
-fi
-
+apt  install docker.io -y
 docker run -d --restart always --name dante -p 7890:1080 -e PROXY_USER=roota1 -e PROXY_PASSWORD=roota2 cpfd/dante

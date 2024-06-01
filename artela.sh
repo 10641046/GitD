841b77516537c04b2d30de342d7acef3a8de35a7
10641046 <106446830+10641046@users.noreply.github.com>
2024-06-01 21:42:18 +0800

    Update artela.sh

--- artela.sh
+++ artela.sh
@@ -8,7 +8,7 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # 检查并安装 Node.js 和 npm
-export  $HOME="/mnt/c1"
+export HOME=/mnt/c1
 
 
 function install_nodejs_and_npm() {
@@ -46,31 +46,6 @@ function check_go_installation() {
     else
         echo "Go 环境未安装，正在安装..."
         return 1 
-    fi
-}
-
-# 自动设置快捷键的功能
-function check_and_set_alias() {
-    local alias_name="art"
-    local shell_rc="$HOME/.bashrc"
-
-    # 对于Zsh用户，使用.zshrc
-    if [ -n "$ZSH_VERSION" ]; then
-        shell_rc="$HOME/.zshrc"
-    elif [ -n "$BASH_VERSION" ]; then
-        shell_rc="$HOME/.bashrc"
-    fi
-
-    # 检查快捷键是否已经设置
-    if ! grep -q "$alias_name" "$shell_rc"; then
-        echo "设置快捷键 '$alias_name' 到 $shell_rc"
-        echo "alias $alias_name='bash $SCRIPT_PATH'" >> "$shell_rc"
-        # 添加提醒用户激活快捷键的信息
-        echo "快捷键 '$alias_name' 已设置。请运行 'source $shell_rc' 来激活快捷键，或重新打开终端。"
-    else
-        # 如果快捷键已经设置，提供一个提示信息
-        echo "快捷键 '$alias_name' 已经设置在 $shell_rc。"
-        echo "如果快捷键不起作用，请尝试运行 'source $shell_rc' 或重新打开终端。"
     fi
 }
 
@@ -136,146 +111,6 @@ function install_node() {
     pm2 restart artelad
 
     echo '====================== 安装完成,请退出脚本后执行 source $HOME/.bash_profile 以加载环境变量 ==========================='
-    
-}
-
-# 查看Artela 服务状态
-function check_service_status() {
-    pm2 list
-}
-
-# Artela 节点日志查询
-function view_logs() {
-    pm2 logs artelad
-}
-
-# 卸载节点功能
-function uninstall_node() {
-    echo "你确定要卸载Artela 节点程序吗？这将会删除所有相关的数据。[Y/N]"
-    read -r -p "请确认: " response
-
-    case "$response" in
-        [yY][eE][sS]|[yY]) 
-            echo "开始卸载节点程序..."
-            pm2 stop artelad && pm2 delete artelad
-            rm -rf $HOME/.artelad $HOME/artela $(which artelad)
-            echo "节点程序卸载完成。"
-            ;;
-        *)
-            echo "取消卸载操作。"
-            ;;
-    esac
-}
-
-# 创建钱包
-function add_wallet() {
-    artelad keys add wallet
-}
-
-# 导入钱包
-function import_wallet() {
-    artelad keys add wallet --recover
-}
-
-# 查询余额
-function check_balances() {
-    read -p "请输入钱包地址: " wallet_address
-    artelad query bank balances "$wallet_address"
-}
-
-# 查看节点同步状态
-function check_sync_status() {
-    artelad status | jq .SyncInfo
-}
-
-# 创建验证者
-function add_validator() {
-    read -p "请输入您的钱包名称: " wallet_name
-    read -p "请输入您想设置的验证者的名字: " validator_name
-    
-artelad tx staking create-validator \
---amount="1art" \
---pubkey=$(artelad tendermint show-validator) \
---moniker="$validator_name" \
---commission-rate="0.10" \
---commission-max-rate="0.20" \
---commission-max-change-rate="0.01" \
---min-self-delegation="1" \
---gas="200000" \
---chain-id="artela_11822-1" \
---from="$wallet_name" \
-
-}
-
-
-# 给自己地址验证者质押
-function delegate_self_validator() {
-read -p "请输入质押代币数量: " math
-read -p "请输入钱包名称: " wallet_name
-artelad tx staking delegate $(artelad keys show $wallet_name --bech val -a)  ${math}art --from $wallet_name --chain-id=artela_11822-1 --gas=300000
-
-}
-
-# 导出验证者key
-function export_priv_validator_key() {
-    echo "====================请将下方所有内容备份到自己的记事本或者excel表格中记录==========================================="
-    cat ~/.artelad/config/priv_validator_key.json
-    
-}
-
-
-function update_script() {
-    SCRIPT_URL="https://raw.githubusercontent.com/a3165458/Artela/main/Artela.sh"
-    curl -o $SCRIPT_PATH $SCRIPT_URL
-    chmod +x $SCRIPT_PATH
-    echo "脚本已更新。请退出脚本后，执行bash Artela.sh 重新运行此脚本。"
-}
-
-# 主菜单
-function main_menu() {
-    while true; do
-        clear
-        echo "脚本以及教程由推特用户大赌哥 @y95277777 编写，免费开源，请勿相信收费"
-        echo "============================Artela节点安装===================================="
-        echo "节点社区 Telegram 群组:https://t.me/niuwuriji"
-        echo "节点社区 Telegram 频道:https://t.me/niuwuriji"
-        echo "节点社区 Discord 社群:https://discord.gg/GbMV5EcNWF"
-        echo "退出脚本，请按键盘ctrl c退出即可"
-        echo "请选择要执行的操作:"
-        echo "1. 安装节点"
-        echo "2. 创建钱包"
-        echo "3. 导入钱包"
-        echo "4. 查看钱包地址余额"
-        echo "5. 查看节点同步状态"
-        echo "6. 查看当前服务状态"
-        echo "7. 运行日志查询"
-        echo "8. 卸载节点"
-        echo "9. 设置快捷键"  
-        echo "10. 创建验证者"  
-        echo "11. 给自己质押" 
-        echo "12. 备份验证者私钥" 
-        echo "13. 更新本脚本" 
-        read -p "请输入选项（1-13）: " OPTION
-
-        case $OPTION in
-        1) install_node ;;
-        2) add_wallet ;;
-        3) import_wallet ;;
-        4) check_balances ;;
-        5) check_sync_status ;;
-        6) check_service_status ;;
-        7) view_logs ;;
-        8) uninstall_node ;;
-        9) check_and_set_alias ;;
-        10) add_validator ;;
-        11) delegate_self_validator ;;
-        12) export_priv_validator_key ;;
-        13) update_script ;;
-        *) echo "无效选项。" ;;
-        esac
-        echo "按任意键返回主菜单..."
-        read -n 1
-    done
     
 }
 

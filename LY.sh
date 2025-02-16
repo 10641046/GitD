175f4373a3bdeafe98371bca470703eb44971221
10641046 <106446830+10641046@users.noreply.github.com>
2025-02-16 19:05:54 +0800

    Update LY.sh

--- LY.sh
+++ LY.sh
@@ -7,7 +7,7 @@ apt  install docker.io -y
 
 docker run -d --restart always --name dante -p 1051:1080 -e PROXY_USER=233blog -e PROXY_PASSWORD=314159mol cpfd/dante
 docker run -d --name tm traffmonetizer/cli_v2 start accept --token FiuWptua5WC3hGsShMq/EF2n4vL23+vrkWKoSj6zZhU= --restart=always
-sudo docker run -d --restart=always -e EARNFM_TOKEN="6afbe6b9-db39-49b5-af32-bf36a8734d80" --name earnfm-client earnfm/earnfm-client:latest
+sudo docker run -d --restart=always -e EARNFM_TOKEN="d9e11358-6ce6-4b4e-8245-52421816a49f" --name earnfm-client earnfm/earnfm-client:latest
 wget https://download.npool.io/npool.sh&&sudo chmod +x npool.sh&&sudo ./npool.sh 2ja04P823ll6yCkm
 
 
